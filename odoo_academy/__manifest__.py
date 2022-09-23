6c1f30c7a27dc1f6ad7f0b2b7803e9cd68adb1a5
David23-bit <david.fuentes@itprove.com.mx>
2022-09-23 18:46:37 +0000

    [ADD] Basic Module

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,30 +1,31 @@
+{
+    'name': 'Odoo Academy',
     
-    {
-        'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+    Academy Module to manage Training:
+    - Courses
+    - Sessions
+    - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+      
+  
+    ],
+
+    'demo': [
         
-        'summary': """Academy app to manage Training""",
-        
-        'description': """
-        Academy Module to manage Training:
-        -Courses
-        -Sessions
-        -Attendees
-        """,
-        
-        'author': 'Odoo',
-        
-        'website': 'https://www.odoo.com',
-        
-        'category': 'Training',
-        
-        'version': '0.1',
-        
-        'depends': ['base'],
-        
-        'data': [
-            
-        ],
-        'demo':[
-            
-        ],
-    }
+    ],
+}
